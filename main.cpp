8e24b3e853b5ad5bf617c1869ea3833c42c6a4fe
marinaioana <115020895+marinaioana@users.noreply.github.com>
2022-12-16 20:45:39 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,20 +4,20 @@
 using namespace std;
 
 int main() {
-    int contatore =0;
-    char nomi [10][20];
-    inizio(nomi,10);
-    for (int i = 0; i < 10; i++) {
-        cin >> nomi[i];
+    int contatore = 0;
+    char nomi[10][20];
+    inizio (nomi, 10);
+    for (int i=0; i<10; i++){
+        cin >> nomi [i];
     }
-   char nomedue [1][20];
-    inizio (nomedue,1);
-    cin >> nomedue [0];
-    if ( funzione (nomi, nomedue, contatore)) {
-        cout<< contatore << endl;
+
+    char nomedue [1][20];
+    inizio (nomedue, 1);
+    cin >> nomedue[0];
+    if (funzione ( nomi, nomedue , contatore)){
+        cout << contatore << endl;
     } else {
-        cout << " non presente" << endl;
+        cout << "non presente" << endl;
     }
     return 0;
 }
-
