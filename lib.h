ecf474ae2a5b91fee6c4898c204b8b16ae99ef2e
marinaioana <115020895+marinaioana@users.noreply.github.com>
2022-12-16 20:45:12 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,3 +1,3 @@
-bool funzione ( char [][20], char [][20], int &contatore);
-bool mm ( char[][20], char [][20], int &contatore);
-void inizio ( char [][20], int m);
+bool funzione (char [][20], char [][20], int &contatore);
+bool ciao (char [][20], char[][20], int &contatore);
+void inizio (char [][20], int m);
