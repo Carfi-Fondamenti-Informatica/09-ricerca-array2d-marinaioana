671cf70bec4f9e0a44aaf88ed7cd3b859941b4c0
marinaioana <115020895+marinaioana@users.noreply.github.com>
2022-12-16 20:44:03 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,6 +1,3 @@
-#ifndef UNTITLED25_LIB_H
-#define UNTITLED25_LIB_H
 bool funzione ( char [][20], char [][20], int &contatore);
 bool mm ( char[][20], char [][20], int &contatore);
 void inizio ( char [][20], int m);
-#endif //UNTITLED25_LIB_H
