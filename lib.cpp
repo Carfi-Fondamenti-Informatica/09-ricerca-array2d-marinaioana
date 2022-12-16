d26b5e2af92de1ae4a89c2e2cc8c8f0db34fb10c
marinaioana <115020895+marinaioana@users.noreply.github.com>
2022-12-16 20:44:47 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,25 +1,28 @@
-bool mm( char nomi [10][10], char nomedue [1][20], int &contatore){
-    for (int j=0; j<20;j++ ){
-        if ( nomi [contatore][j] != nomedue [0][j]){
+bool ciao (char nomi [10][20], char nomedue [1][20], int &contatore) {
+    for (int j=0; j<20; j++){
+        if ( nomi[contatore][j] != nomedue[0][j] ){
             return false;
-        }else{
-            if (j== 19){
+        }else {
+            if (j == 19){
                 return true;
             }
         }
+
     }
 }
-bool funzione (char nomi [10][20], char nomedue [1][20], int &contatore){
-    for (contatore = 0; contatore < 10; contatore++){
-        if ( mm (nomi, nomedue, &contatore)){
+
+bool funzione (char nomi[10][20], char nomedue [1][20], int &contatore){
+    for (contatore = 0; contatore<10; contatore++){
+        if ( ciao (nomi, nomedue, contatore)){
             return true;
-            }
-        }return false;
+        }
+    }return false;
 }
-void inizio (char nomi[][20],int m){
-    for (int i=0; i<m; i++){
-        for (int j=0; j< 20; j++){
-          nomi [i][j]= ' ';
+
+void inizio (char nomi[][20], int m){
+    for (int i =0; i<m;i ++){
+        for (int j=0; j < 20; j++){
+            nomi[i][j]= ' ';
         }
     }
 }
